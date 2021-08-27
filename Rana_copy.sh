1571f8beb7088cc37fcae5e8043c5321d36925c0
Rana-Yehya <elrana112@gmail.com>
2021-08-27 22:01:25 +0200

    Added new file contain the necessary commands for cloning and pushung into the repo by Rana

--- Rana_copy.sh
+++ Rana_copy.sh
@@ -2,7 +2,7 @@ git clone https://github.com/andrewroshdy/lab
 cd lab/
 touch Rana_copy.sh
 git add *
-git commit -m "Added new file contain the necessary commands for creating a new repo by Rana"
-git remote -v
-git remote rename origin destination
-git push destination
+git commit -m "Added new file contain the necessary commands for cloning and pushung into the repo by Rana"
+git push
+
+
